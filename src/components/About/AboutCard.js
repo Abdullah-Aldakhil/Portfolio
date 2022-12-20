2019216ec37cce2d075eaa17a00a4c1d6e0c58bd
Abdullah Aldakhil <abdullahaldakhil@alMacBook-Air-alkhas-bAbdullah.local>
2022-12-20 11:23:05 +0300

    first commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am{" "}
+            <span className="purple"> Abdullah Aldakhil </span>
+            from <span className="purple"> Riyadh, Saudi Arabia.</span>
+            <br />
+            Proficient front-end developer, writing clean and efficient code.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Football
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Particle from "../Particle";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          <strong className="purple">Soon... </strong>
+        </h1>
+      </Container>
+    </Container>
+    
+  );
+}
+
+export default Projects;
